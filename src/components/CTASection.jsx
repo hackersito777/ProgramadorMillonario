2e3ccc6cb9c49c3187eafcd8ee6d21359e40610a
hackersito777 <yefersonjmo777@gmail.com>
2023-12-27 15:37:31 -0500

    Primer commits

--- src/components/CTASection.jsx
+++ src/components/CTASection.jsx
@@ -1,21 +1,16 @@
 // CTASection.jsx
 import React from 'react';
 import './CTASection.css';
-import bonus1 from "./bonus1.jpg"
-import bonus2 from "./bonus2.jpg"
-import bonus3 from "./bonus3.jpg"
+import bonus1 from "./bonus1.jpg";
+import bonus2 from "./bonus2.jpg";
+import bonus3 from "./bonus3.jpg";
 
 const CTASection = () => {
+  const phoneNumber = '573187055383';
 
-  const HeroSection = () => {
-    const phoneNumber = '573187055383';
-  }
-  
-    const generateWhatsAppLink = () => {
-      location.href = `https://wa.me/${phoneNumber}`;
-    }; 
-  
-
+  const generateWhatsAppLink = () => {
+    window.location.href = `https://wa.me/${phoneNumber}`;
+  };
 
   return (
     <section id="cta" className="cta-container">
@@ -26,27 +21,20 @@ const CTASection = () => {
       <div className="bonus-section">
         <h3>Bonus</h3>
         <div className="bonus-images">
-        
-        <div className='imagentainer'>
-        <img src={bonus1} alt="Bonus 1" />
-        <p>Libro "Codigo De Exito - Programador Millonario</p>
-        </div>
+          <div className='imagentainer'>
+            <img src={bonus1} alt="Bonus 1" />
+            <p>Libro "Código De Éxito - Programador Millonario"</p>
+          </div>
 
-        <div className='imagentainer'>
-        <img src={bonus2} alt="Bonus 2" />
-        <p>Mentoria Programador Millonario</p>
-        </div>
+          <div className='imagentainer'>
+            <img src={bonus2} alt="Bonus 2" />
+            <p>Mentoría Programador Millonario</p>
+          </div>
 
-        <div className='imagentainer'>
-        <img src={bonus3} alt="Bonus 2" />
-        <p>Comunidad De Telegram Programador Millonario</p>
-        </div>
-
-          
-          
-          
-
-          
+          <div className='imagentainer'>
+            <img src={bonus3} alt="Bonus 3" />
+            <p>Comunidad De Telegram Programador Millonario</p>
+          </div>
         </div>
       </div>
     </section>
